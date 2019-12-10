e4c5268d28107ea4d8557e2a9218900c20f7986d
Alexander Froehlich <Alexander.Froehlich@desy.de>
2019-12-10 16:37:20 +0100

    add event class to hold information of collision event

---
+++ Event.py
@@ -0,0 +1,30 @@
+from uhhObjects import *
+
+class Event(object):
+    """
+    A collision event. Holding all important physics object required for the LHC analysis.
+    """
+    def __init__(self):
+        self.muons = []
+        self.jets = []
+        self.met = None
+        self.trigger = {}
+        self.weight = 1.0
+
+    def n_jets(self):
+        """
+        returns number of jets.
+        """
+        return len(self.jets)
+    
+    def n_muons(self):
+        """
+        returns number of muons.
+        """
+        return len(self.muons)
+
+    def n_b_jets(self):
+        """
+        returns number of b-tagged jets.
+        """
+        return len([jet for jet in self.jets if jet.has_b_tag])
