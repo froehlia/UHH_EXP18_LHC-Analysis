6ddc8a6a50814a4666ce267cebe6bb7dfc2e0b7f
Alexander Froehlich <Alexander.Froehlich@desy.de>
2020-01-28 14:51:53 +0100

    update test analyzer and combine diboson root files

--- test_analysis.py
+++ test_analysis.py
@@ -1,14 +1,23 @@
 from TTbarAnalyzer import TTbarAnalyzer
-
+from Plotter import Plotter
+from collections import OrderedDict
 
 if __name__ == "__main__":
-    datasets = {'TTbar': 'ttbar.root'}
+    datasets = OrderedDict([('Data', 'data.root'),
+                            ('QCD', 'qcd.root'),
+                            ('Diboson', 'diboson.root'),
+                            ('DY+jets', 'dy.root'),
+                            ('single top', 'single_top.root'),
+                            ('TTbar', 'ttbar.root'), 
+                            ('W+jets', 'wjets.root'),
+                            ]
+                           )
 
     event_options = {'JEC': 'nominal',
                      'muon_isolation': 0.1
                      }
     
-    analyzers = {}
+    analyzers = OrderedDict()
     for name, file_name in datasets.iteritems():
         analyzer = TTbarAnalyzer(name, file_name, event_options)
         # run analysis for dataset
@@ -16,3 +25,5 @@ if __name__ == "__main__":
         analyzers[name] = analyzer
 
     print analyzers['TTbar'].n_total
+    plotter = Plotter(analyzers)
+    plotter.process()
